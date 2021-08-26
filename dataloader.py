3870dcbe935e444abae6096fc5b1d68384dc236b
anwarvic <mohamedanwarvic@gmail.com>
2021-08-26 17:58:20 +0000

    edited .gitignore

--- dataloader.py
+++ dataloader.py
@@ -49,7 +49,7 @@ def get_dataloader(data, src_transform, tgt_transform, conf):
         for src_sample, tgt_sample in batch:
             src_batch.append(src_transform(src_sample.rstrip("\n")))
             tgt_batch.append(tgt_transform(tgt_sample.rstrip("\n")))
-        src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
-        tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
+        src_batch = pad_sequence(src_batch, padding_value=PAD_IDX, batch_first=True)
+        tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX, batch_first=True)
         return src_batch, tgt_batch
     return DataLoader(data, batch_size=conf["batch_size"], collate_fn=collate_fn)

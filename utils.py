7f7def65c3abc3f71dca0ac7907fd6b9f6f056f0
anwarvic <mohamedanwarvic@gmail.com>
2021-08-26 17:58:54 +0000

    added more helpful functions to the utils.py script

--- utils.py
+++ utils.py
@@ -23,3 +23,9 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs

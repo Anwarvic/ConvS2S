46376e73ff1a2f89a284f67c91898d38d949e362
anwarvic <mohamedanwarvic@gmail.com>
2021-08-27 00:49:35 +0000

    moved tqdm to batches instead of epochs

--- train.py
+++ train.py
@@ -1,15 +1,16 @@
+import os
 import time
 import math
 import torch
 from tqdm import tqdm
 
-from utils import epoch_time
+from utils import epoch_time, save_model
 
 
-def train_epoch(model, iterator, optimizer, criterion, clip):
+def train_epoch(model, data_loader, optimizer, criterion, clip, epoch):
     model.train()
     epoch_loss = 0
-    for src, tgt in iterator:
+    for src, tgt in tqdm(data_loader, total=len(data_loader), desc=f"Training {epoch+1}"):
         optimizer.zero_grad()
         output, _ = model(src, tgt[:,:-1])
         output_dim = output.shape[-1]
@@ -20,27 +21,27 @@ def train_epoch(model, iterator, optimizer, criterion, clip):
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
         epoch_loss += loss.item()
-    return epoch_loss / len(iterator)
+    return epoch_loss / len(data_loader)
 
-def evaluate(model, iterator, criterion):
+def evaluate(model, data_loader, criterion):
     model.eval()
     epoch_loss = 0
     with torch.no_grad():
-        for src, tgt in tqdm(iterator, "Evaluating"):
+        for src, tgt in tqdm(data_loader, total=len(data_loader), desc="Evaluating"):
             output, _ = model(src, tgt[:,:-1])
             output_dim = output.shape[-1]
             output = output.contiguous().view(-1, output_dim)
             tgt = tgt[:,1:].contiguous().view(-1)
             loss = criterion(output, tgt)
             epoch_loss += loss.item()
-    return epoch_loss / len(iterator)
+    return epoch_loss / len(data_loader)
 
 
 def train(model, criterion, train_dataloader, valid_dataloader, optimizer, num_epochs, clip):
     best_valid_loss = float('inf')
-    for epoch in tqdm(range(num_epochs), "Training"):
+    for epoch in range(num_epochs):
         start_time = time.time()
-        train_loss = train_epoch(model, train_dataloader, optimizer, criterion, clip)
+        train_loss = train_epoch(model, train_dataloader, optimizer, criterion, clip, epoch)
         valid_loss = evaluate(model, valid_dataloader, criterion)
         end_time = time.time()
         
@@ -48,7 +49,7 @@ def train(model, criterion, train_dataloader, valid_dataloader, optimizer, num_e
         
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
-            torch.save(model.state_dict(), 'bestmodel.pt')
+            save_model(model)
         
         print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
         print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
